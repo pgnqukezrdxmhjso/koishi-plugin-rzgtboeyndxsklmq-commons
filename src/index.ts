30e607bd8a6f7f4dd69ed277b7dab6aaa8dce0f3
173gvQY8MFaDk9Jt <g@v.q>
2026-01-25 04:31:12 +0800

    Test.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from "koishi";
 
-export * from './utils/BeanHelper'
-export * from './utils/File'
+export * from "./utils/BeanHelper";
+export * from "./utils/File";
+export * from "./utils/Test";
 
-export const name = 'rzgtboeyndxsklmq-commons'
+export const name = "rzgtboeyndxsklmq-commons";
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
-export function apply(ctx: Context, config: Config) {
-}
+export function apply(ctx: Context, config: Config) {}
--- src/utils/File.ts
+++ src/utils/File.ts
@@ -3,19 +3,19 @@ import path from "node:path";
 
 export async function readDirFiles(dirPath: string, needDir: boolean = false) {
   const files = await fs.readdir(dirPath, {
+    recursive: true,
     withFileTypes: true,
   });
   const paths: string[] = [];
-  files.sort((a, b) => (a.isFile() ? 1 : 0) - (b.isFile() ? 1 : 0));
-  for (const file of files) {
+  files.forEach((file) => {
     if (file.isDirectory()) {
-      if (needDir) {
-        paths.push(path.join(path.resolve(dirPath, file.name), path.sep));
+      if (!needDir) {
+        return;
       }
-      paths.push(...(await readDirFiles(path.join(dirPath, file.name))));
+      paths.push(path.join(file.parentPath, file.name, path.sep));
     } else if (file.isFile()) {
-      paths.push(path.resolve(dirPath, file.name));
+      paths.push(path.join(file.parentPath, file.name));
     }
-  }
+  });
   return paths;
 }
